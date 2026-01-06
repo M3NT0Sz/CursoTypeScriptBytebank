ad16b8e4f71e48663c212ac1437fe0966e3689c6
Matheus Mendes <103116527+M3NT0Sz@users.noreply.github.com>
2026-01-05 23:04:28 -0300

    Definindo Tipos

---
+++ dist/js/typescript.js
@@ -0,0 +1,20 @@
+let valor = 3000;
+let nome = "Matheus";
+let isPago = false;
+let qualquer = "";
+qualquer = 22;
+// Arrays
+const lista = [];
+lista.push(11, 22, 33, 44);
+// Enum
+var TipoTransacao;
+(function (TipoTransacao) {
+    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
+    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
+    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
+})(TipoTransacao || (TipoTransacao = {}));
+const novaTransacao = {
+    tipoTransacao: TipoTransacao.DEPOSITO,
+    data: new Date(),
+    valor: 0,
+};
